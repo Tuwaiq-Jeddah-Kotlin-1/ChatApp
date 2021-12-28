cfbbd56675440bc2040a8d6173137c72891b8088
musab <mas.gh123@hotmail.com>
2021-12-28 16:20:14 +0300

    1) Create chat list adapter: ChatListAdapter.kt 2) time => timeStamp in Message.kt

---
+++ app/src/main/java/com/myprojects/chatapp/ChatListAdapter.kt
@@ -0,0 +1,36 @@
+package com.myprojects.chatapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.myprojects.chatapp.models.ChatList
+
+class ChatListAdapter(val chatList: ArrayList<ChatList>) : RecyclerView.Adapter<ChatListAdapter.ChatViewHolder>() {
+    class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val chatPicture = itemView.findViewById<ImageView>(R.id.chatPictureIV)
+        val chatTitle = itemView.findViewById<TextView>(R.id.chatTitleTV)
+        val chatLastMessage = itemView.findViewById<TextView>(R.id.chatLastMessageTV)
+        val chatLastActivityTime = itemView.findViewById<TextView>(R.id.chatLastActivityTime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.chat_list_item,parent,false)
+
+        return ChatViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        holder.chatTitle.text = chatList[position].title
+        holder.chatLastMessage.text = chatList[position].lastMessage
+        holder.chatLastActivityTime.text = chatList[position].lastActivityTime
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+
+
+}
