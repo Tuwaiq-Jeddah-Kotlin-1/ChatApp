eb384dc863c659d86986e307177459854ae54f05
musab <mas.gh123@hotmail.com>
2022-01-16 22:33:11 +0300

    Update Message.kt to make it more compatible with firestore!

--- app/src/main/java/com/myprojects/chatapp/models/Message.kt
+++ app/src/main/java/com/myprojects/chatapp/models/Message.kt
@@ -1,3 +1,9 @@
 package com.myprojects.chatapp.models
 
-data class Message(val message: String, val senderId: String, val timeStamp: String)
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
+data class Message(val message: String = "",
+                   val senderId: String = "",
+                   @ServerTimestamp
+                   val sentAt: Date? = null)
