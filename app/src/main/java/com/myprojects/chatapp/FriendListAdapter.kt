8d75716456a309a04fb4df8be31dc17497d42350
musab <mas.gh123@hotmail.com>
2022-01-19 20:55:05 +0300

    safe args takes both sender receiver id

--- app/src/main/java/com/myprojects/chatapp/FriendListAdapter.kt
+++ app/src/main/java/com/myprojects/chatapp/FriendListAdapter.kt
@@ -9,9 +9,11 @@ import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.myprojects.chatapp.models.User
 
 class FriendListAdapter(val friendList: ArrayList<User>) : RecyclerView.Adapter<FriendListAdapter.FriendViewHolder>() {
+    private val currentUid = FirebaseAuth.getInstance().uid
     inner class FriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val profilePic = itemView.findViewById<ImageView>(R.id.friendProfilePicIV)
         val friendUserName = itemView.findViewById<TextView>(R.id.friendUserNameTV)
@@ -20,7 +22,8 @@ class FriendListAdapter(val friendList: ArrayList<User>) : RecyclerView.Adapter<
             itemView.setOnClickListener(this)
         }
         override fun onClick(view: View?) {
-            val argument = bundleOf("friend" to friendList[position])
+            val argument = bundleOf("fromId" to currentUid,
+                "toId" to friendList[position].id)
             Navigation.findNavController(view!!).navigate(R.id.chatFragment,argument)
         }
     }
