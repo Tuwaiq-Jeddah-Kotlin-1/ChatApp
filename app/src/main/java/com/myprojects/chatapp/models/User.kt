a704319287738313ca77a00acebcb9b6e632c33d
musab <mas.gh123@hotmail.com>
2022-01-19 20:51:16 +0300

    Add chatRooms property to User.kt

--- app/src/main/java/com/myprojects/chatapp/models/User.kt
+++ app/src/main/java/com/myprojects/chatapp/models/User.kt
@@ -1,15 +1,12 @@
 package com.myprojects.chatapp.models
 
-import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import java.util.*
 
-@Parcelize
+
 data class User(val id: String = "",
                 val userName: String = "",
                 val userEmail: String = "",
                 val profilePicture: String? = "",
-                val memberSince: String = "") : Parcelable{
-                    //constructor(): this("","","","",null,"")
-                }
+                val chatRooms: MutableMap<String, Any>? = null,
+                val memberSince: String = "")
