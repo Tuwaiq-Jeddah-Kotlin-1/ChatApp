ed536e539de3f5e7dad114f96c61819d0a60e6b9
musab <mas.gh123@hotmail.com>
2022-01-31 01:32:19 +0300

    In ChatFragment.kt: Fix sendButton.setOnClickListener to update "chats" in firestore and make it hide the soft keyboard

--- app/src/main/java/com/myprojects/chatapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/myprojects/chatapp/fragments/ChatFragment.kt
@@ -1,33 +1,33 @@
 package com.myprojects.chatapp.fragments
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.ktx.toObject
 import com.myprojects.chatapp.MainActivity
 import com.myprojects.chatapp.MessageAdapter
 import com.myprojects.chatapp.R
+import com.myprojects.chatapp.models.ChatList
 import com.myprojects.chatapp.models.Message
 import com.myprojects.chatapp.models.User
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 import kotlin.collections.ArrayList
-import kotlin.math.log
 
 
 class ChatFragment : Fragment() {
 
+    private lateinit var bottomNav: BottomNavigationView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageButton
     private lateinit var messagesRecyclerView: RecyclerView
@@ -49,6 +49,8 @@ class ChatFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_chat, container, false)
 
+        bottomNav = requireActivity().findViewById(R.id.mainBottomNav)
+        bottomNav.visibility = View.GONE
 
         senderReceiver = getSenderAndReceiver()
 
@@ -59,22 +61,21 @@ class ChatFragment : Fragment() {
 
         messages = ArrayList()
 
-        val m1 = Message("Hello world",senderId,null)
-        val m2 = Message("dont hello me",receiverId,null)
+        val m1 = Message("Hello world", senderId, null)
+        val m2 = Message("dont hello me", receiverId, null)
 
-        val mList = arrayListOf(m1,m2)
-
+        val mList = arrayListOf(m1, m2)
 
 
         var senderRooms = senderReceiver[0]?.chatRooms
         var receiverRooms = senderReceiver[1]?.chatRooms
 
-        if (roomId == "noRoomId"){
+        if (roomId == "noRoomId") {
             roomId = chatsCollectionRef.document().id
-            if(senderRooms != null){
-                for ((key, _) in senderRooms){
-                    if (receiverRooms != null){
-                        if (receiverRooms.contains(key)){
+            if (senderRooms != null) {
+                for ((key, _) in senderRooms) {
+                    if (receiverRooms != null) {
+                        if (receiverRooms.contains(key)) {
                             roomId = key
                         }
                     }
@@ -91,49 +92,75 @@ class ChatFragment : Fragment() {
 
         //messagesRecyclerView.adapter = MessageAdapter(mList)
 
-            GlobalScope.launch {
+        GlobalScope.launch {
 
         }
         updateChat()
 
         sendButton.setOnClickListener {
-            if (senderRooms == null){
+            if (senderRooms == null) {
                 senderRooms = mutableMapOf()
             }
+
+            val messageText = messageBox.text.toString()
+            val message = Message(messageText, senderId)
+
+            val senderChatList = ChatList(
+                roomId,
+                senderReceiver[0].id,
+                senderReceiver[0].userName,
+                messageText,
+                System.currentTimeMillis().toString()
+            )
+            val receiverChatList = ChatList(
+                roomId,
+                senderReceiver[1].id,
+                senderReceiver[1].userName,
+                messageText,
+                System.currentTimeMillis().toString()
+            )
+
             senderRooms?.set(roomId, true)
             senderReceiver[0]?.chatRooms = senderRooms
-            senderReceiver[0]?.let { rootRef.collection("users").document(senderId).set(it, SetOptions.merge()) }
+            senderReceiver[0]?.let {
+                rootRef.collection("users").document(senderId).set(it, SetOptions.merge())
+            }
             senderReceiver[1]?.let {
                 rootRef.collection("friends").document(senderId)
                     .collection("friendList").document(receiverId).set(it, SetOptions.merge())
             }
-            senderReceiver[1]?.let {
-                rootRef.collection("chats").document(senderId)
-                    .collection("userChatRooms").document(roomId).set(it, SetOptions.merge()) }
 
-            if (receiverRooms == null){
+            rootRef.collection("chats").document(senderId)
+                .collection("userChatRooms").document(roomId).set(senderChatList)
+
+            if (receiverRooms == null) {
                 receiverRooms = mutableMapOf()
             }
             receiverRooms?.set(roomId, true)
             senderReceiver[1]?.chatRooms = receiverRooms
-            senderReceiver[1]?.let { rootRef.collection("users").document(receiverId).set(it, SetOptions.merge()) }
+            senderReceiver[1]?.let {
+                rootRef.collection("users").document(receiverId).set(it, SetOptions.merge())
+            }
             senderReceiver[0]?.let {
                 rootRef.collection("friends").document(receiverId)
                     .collection("friendList").document(senderId).set(it, SetOptions.merge())
             }
-            senderReceiver[0]?.let {
                 rootRef.collection("chats").document(receiverId)
-                    .collection("userChatRooms").document(roomId).set(it, SetOptions.merge()) }
+                    .collection("userChatRooms").document(roomId).set(receiverChatList)
 
-            val messageText = messageBox.text.toString()
-            val message = Message(messageText, senderId)
+
+
             rootRef.collection("messages")
-                .document(roomId).collection("roomMessages").document(System.currentTimeMillis().toString()).set(message)
+                .document(roomId).collection("roomMessages")
+                .document(System.currentTimeMillis().toString()).set(message)
+
+//            rootRef.collection("chats").
             GlobalScope.launch {
 
                 //updateChat()
             }
             messageBox.text.clear()
+            it.hideKeyboard()
         }
 
 
@@ -141,43 +168,36 @@ class ChatFragment : Fragment() {
     }
 
 
-    private fun getSender(): User {
-        return arguments?.get("fromUser") as User
+    private fun getSenderId(): String {
+        return arguments?.get("senderId") as String
     }
 
-    private fun getReceiver(): User {
-        return arguments?.get("toUser") as User
+    private fun getReceiverId(): String {
+        return arguments?.get("receiverId") as String
     }
 
-    private fun updateChat(){
-        messagesCollectionRef.document(roomId).collection("roomMessages").addSnapshotListener { snapshots, e ->
-            if (snapshots != null) {
-                messages.clear()
-                for (snap in snapshots){
+    fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
+    }
 
-                    messages.add(snap.toObject(Message::class.java))
+    private fun updateChat() {
+        messagesCollectionRef.document(roomId).collection("roomMessages")
+            .addSnapshotListener { snapshots, e ->
+                if (snapshots != null) {
+                    messages.clear()
+                    for (snap in snapshots) {
+
+                        messages.add(snap.toObject(Message::class.java))
+                    }
+                    messagesRecyclerView.adapter = MessageAdapter(messages)
                 }
-                Log.d("5555555555555555555", "${messages.size}")
-
-                messagesRecyclerView.adapter = MessageAdapter(messages)
             }
-        }
     }
 
 
-//    private suspend fun getMessageList() {
-//        messagesCollectionRef.document(roomId).collection("roomMessages").get().addOnCompleteListener { snapshot ->
-//            if (snapshot.isSuccessful) {
-//                for (snap in snapshot.result!!){
-//                    val message = snap.toObject(Message::class.java)
-//                    messages.add(message)
-//                }
-//                messagesRecyclerView.adapter = MessageAdapter(messages)
-//            }
-//        }.await()
-//    }
 
-    private fun getSenderAndReceiver(): ArrayList<User>{
+    private fun getSenderAndReceiver(): ArrayList<User> {
         val senderReceiverList = ArrayList<User>()
         senderReceiverList.add((activity as MainActivity).getCurrentUser())//userCollectionRef.document(getSenderId()).get().result?.toObject(User::class.java)
         senderReceiverList.add(requireArguments().get("toUser") as User)//userCollectionRef.document(getReceiverId()).get().result?.toObject(User::class.java)
