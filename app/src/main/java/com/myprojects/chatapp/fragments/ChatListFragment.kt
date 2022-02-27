892b922afeebc2973bdc61ba87834d36f19652e0
musab <mas.gh123@hotmail.com>
2022-02-27 21:27:56 +0300

    Move fetching chats list to ChatListViewModel.kt

--- app/src/main/java/com/myprojects/chatapp/fragments/ChatListFragment.kt
+++ app/src/main/java/com/myprojects/chatapp/fragments/ChatListFragment.kt
@@ -1,21 +1,20 @@
 package com.myprojects.chatapp.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.myprojects.chatapp.adapters.ChatListAdapter
 import com.myprojects.chatapp.R
-import com.myprojects.chatapp.models.ChatList
+import com.myprojects.chatapp.viewmodels.ChatListViewModel
+
 
 
 class ChatListFragment : Fragment() {
@@ -23,9 +22,7 @@ class ChatListFragment : Fragment() {
 
     private lateinit var bottomNav: BottomNavigationView
     private lateinit var chatListRV: RecyclerView
-    private lateinit var chatList: ArrayList<ChatList>
-    private val uid = FirebaseAuth.getInstance().uid
-    private val chatsCollectionRef = Firebase.firestore.collection("chats")
+    private lateinit var viewModel: ChatListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,9 +30,9 @@ class ChatListFragment : Fragment() {
     ): View? {
         (activity as AppCompatActivity).supportActionBar?.show()
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.chats_fragment)
-        // Inflate the layout for this fragment
-        chatList = ArrayList()
 
+        viewModel = ViewModelProvider(this).get(ChatListViewModel::class.java)
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list_chat, container, false)
         bottomNav = requireActivity().findViewById(R.id.mainBottomNav)
         bottomNav.visibility = View.VISIBLE
@@ -48,18 +45,9 @@ class ChatListFragment : Fragment() {
         return view
     }
 
-    private fun loadChatList(){
-        chatsCollectionRef.document(uid!!)
-            .collection("userChatRooms")
-            .get()
-            .addOnCompleteListener { snapshot ->
-                if (snapshot.isSuccessful){
-                    for (snap in snapshot.result!!){
-                        chatList.add(snap.toObject(ChatList::class.java))
-                    }
-                    Log.d("00000000000000000000", "$chatList")
-                    chatListRV.adapter = ChatListAdapter(chatList)
-                }
-            }
+    private fun loadChatList() {
+        viewModel.getChatList().observe(viewLifecycleOwner, { list ->
+            chatListRV.adapter = ChatListAdapter(list)
+        })
     }
 }
--- app/src/main/java/com/myprojects/chatapp/viewmodels/ChatListViewModel.kt
+++ app/src/main/java/com/myprojects/chatapp/viewmodels/ChatListViewModel.kt
@@ -1,4 +1,36 @@
 package com.myprojects.chatapp.viewmodels
 
-class ChatListViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.myprojects.chatapp.models.ChatList
+
+class ChatListViewModel : ViewModel() {
+    private val chatList = MutableLiveData<ArrayList<ChatList>>()
+
+    init {
+        chatList.value = ArrayList()
+    }
+
+    fun getChatList(): LiveData<ArrayList<ChatList>> {
+        val uid = FirebaseAuth.getInstance().uid
+        Firebase.firestore
+            .collection("chats")
+            .document(uid!!)
+            .collection("userChatRooms")
+            .get()
+            .addOnCompleteListener { snapshot ->
+                if (snapshot.isSuccessful) {
+                    for (snap in snapshot.result!!) {
+                        chatList.value?.clear()
+                        chatList.value?.add(snap.toObject(ChatList::class.java))
+                        chatList.postValue(chatList.value)
+                    }
+                }
+            }
+        return chatList
+    }
 }
