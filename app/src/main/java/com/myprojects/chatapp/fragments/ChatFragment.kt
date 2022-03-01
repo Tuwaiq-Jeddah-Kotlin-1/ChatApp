40c7cfc29993b34c489e2ccc1026a938c34f3fc8
musab <mas.gh123@hotmail.com>
2022-03-01 16:37:32 +0300

    Move fetching chat messages to ChatViewModel.kt

--- app/src/main/java/com/myprojects/chatapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/myprojects/chatapp/fragments/ChatFragment.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,6 +23,7 @@ import com.myprojects.chatapp.viewmodels.SharedViewModel
 import com.myprojects.chatapp.models.ChatList
 import com.myprojects.chatapp.models.Message
 import com.myprojects.chatapp.models.User
+import com.myprojects.chatapp.viewmodels.ChatViewModel
 import kotlin.collections.ArrayList
 
 
@@ -31,17 +33,16 @@ class ChatFragment : Fragment() {
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageButton
     private lateinit var messagesRecyclerView: RecyclerView
-    private lateinit var adapter: MessageAdapter
-    private lateinit var messages: ArrayList<Message>
     private lateinit var senderId: String
     private lateinit var receiverId: String
     private lateinit var roomId: String
     private lateinit var senderReceiver: ArrayList<User>
+    private lateinit var senderRooms: MutableMap<String, Any>
+    private lateinit var receiverRooms: MutableMap<String, Any>
     private val rootRef = FirebaseFirestore.getInstance()
     private val chatsCollectionRef = rootRef.collection("chats")
-    private val messagesCollectionRef = rootRef.collection("messages")
-    private val userCollectionRef = rootRef.collection("users")
     private val sharedViewModel: SharedViewModel by activityViewModels()
+    private lateinit var chatVM: ChatViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,39 +56,21 @@ class ChatFragment : Fragment() {
         bottomNav = requireActivity().findViewById(R.id.mainBottomNav)
         bottomNav.visibility = View.GONE
 
-        senderReceiver = getSenderAndReceiver()
-
-        senderId = senderReceiver[0].id
-        receiverId = senderReceiver[1].id
-
-        roomId = arguments?.get("roomId") as String
-
-        messages = ArrayList()
-
-        var senderRooms = senderReceiver[0]?.chatRooms
-        var receiverRooms = senderReceiver[1]?.chatRooms
-
-        if (roomId == "noRoomId") {
-            roomId = chatsCollectionRef.document().id
-            if (senderRooms != null) {
-                for ((key, _) in senderRooms) {
-                    if (receiverRooms != null) {
-                        if (receiverRooms.contains(key)) {
-                            roomId = key
-                        }
-                    }
-                }
-            }
-        }
+        chatVM = ViewModelProvider(this).get(ChatViewModel::class.java)
 
 
+        initFields()
+
+        setRoomId()
+
+        chatVM.setChatMessages(roomId)
 
         messageBox = view.findViewById(R.id.sendMessageET)
         sendButton = view.findViewById(R.id.sendIB)
         messagesRecyclerView = view.findViewById(R.id.messageRV)
         messagesRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        updateChat()
+        getChatMessages()
 
         sendButton.setOnClickListener {
             if (senderRooms == null) {
@@ -154,13 +137,16 @@ class ChatFragment : Fragment() {
         return view
     }
 
+    private fun initFields() {
+        senderReceiver = getSenderAndReceiver()
 
-    private fun getSenderId(): String {
-        return arguments?.get("senderId") as String
-    }
+        senderId = senderReceiver[0].id
+        receiverId = senderReceiver[1].id
 
-    private fun getReceiverId(): String {
-        return arguments?.get("receiverId") as String
+        roomId = arguments?.get("roomId") as String
+
+        senderRooms = senderReceiver[0].chatRooms!!
+        receiverRooms = senderReceiver[1].chatRooms!!
     }
 
     fun View.hideKeyboard() {
@@ -168,18 +154,10 @@ class ChatFragment : Fragment() {
         inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 
-    private fun updateChat() {
-        messagesCollectionRef.document(roomId).collection("roomMessages")
-            .addSnapshotListener { snapshots, e ->
-                if (snapshots != null) {
-                    messages.clear()
-                    for (snap in snapshots) {
-
-                        messages.add(snap.toObject(Message::class.java))
-                    }
-                    messagesRecyclerView.adapter = MessageAdapter(messages)
-                }
-            }
+    private fun getChatMessages() {
+        chatVM.getChatMessages().observe(viewLifecycleOwner,{messageList ->
+            messagesRecyclerView.adapter = MessageAdapter(messageList)
+        })
     }
 
 
@@ -187,10 +165,21 @@ class ChatFragment : Fragment() {
     private fun getSenderAndReceiver(): ArrayList<User> {
         val senderReceiverList = ArrayList<User>()
         senderReceiverList.add(sharedViewModel.getCurrentUser().value!!)
-        senderReceiverList.add(requireArguments().get("toUser") as User)//userCollectionRef.document(getReceiverId()).get().result?.toObject(User::class.java)
+        senderReceiverList.add(requireArguments().get("toUser") as User)
 
         return senderReceiverList
     }
 
+
+    fun setRoomId(){
+        if (roomId == "noRoomId") {
+            roomId = chatsCollectionRef.document().id
+            for ((key, _) in senderRooms) {
+                if (receiverRooms.contains(key)) {
+                    roomId = key
+                }
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/myprojects/chatapp/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/myprojects/chatapp/viewmodels/ChatViewModel.kt
@@ -1,4 +1,36 @@
 package com.myprojects.chatapp.viewmodels
 
-class ChatViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.myprojects.chatapp.models.Message
+
+class ChatViewModel : ViewModel() {
+    private val messages = MutableLiveData<ArrayList<Message>>()
+
+    init {
+        messages.value = ArrayList()
+    }
+
+    fun setChatMessages(roomId: String) {
+        Firebase.firestore
+            .collection("messages")
+            .document(roomId)
+            .collection("roomMessages")
+            .addSnapshotListener { snapshot, e ->
+                if (snapshot != null) {
+                    messages.value?.clear()
+                    for (snap in snapshot) {
+                        messages.value?.add(snap.toObject(Message::class.java))
+                        messages.postValue(messages.value)
+                    }
+                }
+            }
+    }
+
+    fun getChatMessages(): LiveData<ArrayList<Message>> {
+        return messages
+    }
 }
